4856c31e731d6adf0b27ff0af8ce2c2baf691dea
Kinga Tos <toskinga@interia.pl>
2023-01-27 18:10:15 +0100

    created the function app and import any components to app.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import Container from './Container';
+import Header from './Header';
+import Section from "./Section"
+import Form from "./Form";
+import BurgerMenu from './BurgerMenu';
+
 
 function App() {
+  const [visits, setVisits] = useState([]);
+  
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const formData = new FormData(event.target);
+    const newVisit = {
+      id: Date.now(),
+      visitDate: formData.get('visit-date'),
+      weight: formData.get('weight'),
+      biceps: formData.get('biceps'),
+      chest: formData.get('chest'),
+      waist: formData.get('waist')
+    };
+    setVisits([...visits, newVisit]);
+  };
+
+  const removeVisit = (id) => {
+    setVisits(visits => visits.filter(visit => visit.id !== id));
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <BurgerMenu />
+    <Container>
+      <Header
+        title="Gym Tracker"
+        subtitle="know your body" />
+      <div>
+        <Form
+          onSubmit={handleSubmit} />
+        <br />
+        <Section
+          visits={visits}
+          removeVisit={removeVisit} />
+      </div>
+    </Container>
+    </>
   );
 }
 
