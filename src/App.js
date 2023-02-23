500442292dd826b8246469a5355e2a39bc83c429
Kinga Tos <toskinga@interia.pl>
2023-02-23 22:14:03 +0100

    modificated code, formatted and used function from react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,64 +1,66 @@
-import { useState, useEffect } from 'react';
-import Container from './Container';
-import Header from './Header';
-import Section from "./Section"
-import Form from "./Form";
-import BurgerMenu from './BurgerMenu';
+import { useState, useEffect } from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import BMIcalculatorPage from "./BMIcalculatorPage";
+import Container from "./Container";
+import Header from "./Header";
+import Section from "./Section";
+import FormPage from "./FormPage";
+import BurgerMenu from "./BurgerMenu";
 import trackerLogo from "./Header/images/trackerlogo.png";
-import BMIcalculator from "./BMIcalculator";
 
-const getInitialTasks = () => {
+
+
+const getInitialVisits = () => {
   const visitsFromLocalStorage = localStorage.getItem("visits");
 
-  return visitsFromLocalStorage
-  ? JSON.parse(visitsFromLocalStorage)
-  : [];
+  return visitsFromLocalStorage && visitsFromLocalStorage.length > 0
+    ? JSON.parse(visitsFromLocalStorage)
+    : [];
 };
 
-
 function App() {
-  const [visits, setVisits] = useState(getInitialTasks);
+  const [visits, setVisits] = useState(getInitialVisits);
 
   useEffect(() => {
     localStorage.setItem("visits", JSON.stringify(visits));
   }, [visits]);
-  
+
   const handleSubmit = (event) => {
     event.preventDefault();
     const formData = new FormData(event.target);
     const newVisit = {
       id: Date.now(),
-      visitDate: formData.get('visit-date'),
-      weight: formData.get('weight'),
-      biceps: formData.get('biceps'),
-      chest: formData.get('chest'),
-      waist: formData.get('waist')
+      visitDate: formData.get("visit-date"),
+      weight: formData.get("weight"),
+      biceps: formData.get("biceps"),
+      chest: formData.get("chest"),
+      waist: formData.get("waist"),
     };
     setVisits([...visits, newVisit]);
   };
 
   const removeVisit = (id) => {
-    setVisits(visits => visits.filter(visit => visit.id !== id));
+    setVisits((visits) => visits.filter((visit) => visit.id !== id));
   };
 
   return (
     <>
-    <BurgerMenu />
-    <Container>
-      <Header
-        trackerLogo={trackerLogo}
-        subtitle="know your body" />
-      <div>
-        <Form
-          onSubmit={handleSubmit} />
-        <br />
-        <Section
-          visits={visits}
-          removeVisit={removeVisit} />
-      </div>
-      <BMIcalculator />
-    </Container>
-    </>
+        <Container>
+          <Header trackerLogo={trackerLogo} subtitle="know your body" />
+          <div>
+            <FormPage onSubmit={handleSubmit} />
+            <br />
+            <Section visits={visits} removeVisit={removeVisit} />
+          </div>
+          <Router>
+        <Routes>
+          <Route path="/" element={<FormPage />} />
+          <Route path="/bmi-calculator" element={<BMIcalculatorPage />} />
+        </Routes>
+        <BurgerMenu />
+        </Router>
+        </Container>
+      </>
   );
 }
 
---
+++ src/FormPage/index.js
@@ -0,0 +1,40 @@
+import "./style.css";
+
+const FormPage = ({ onSubmit }) => (
+  <form className="form" onSubmit={onSubmit}>
+    <label className="label" htmlFor="visit-date">
+      Visit Date:
+    </label>
+    <input
+      className="input"
+      type="date"
+      id="visit-date"
+      name="visit-date"
+      required
+    />
+    <br />
+    <label className="label" htmlFor="weight">
+      Weight:
+    </label>
+    <input className="input" type="number" id="weight" name="weight" required />
+    <br />
+    <label className="label" htmlFor="biceps">
+      Biceps:
+    </label>
+    <input className="input" type="number" id="biceps" name="biceps" required />
+    <br />
+    <label className="label" htmlFor="chest">
+      Chest:
+    </label>
+    <input className="input" type="number" id="chest" name="chest" required />
+    <br />
+    <label className="label" htmlFor="waist">
+      Waist:
+    </label>
+    <input className="input" type="number" id="waist" name="waist" required />
+    <br />
+    <input className="buttonInput" type="submit" value="Add Visit" />
+  </form>
+);
+
+export default FormPage;
