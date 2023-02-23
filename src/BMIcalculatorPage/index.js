ed22cc0ab6fb40b7927325f066c6af167fcc0457
Kinga Tos <toskinga@interia.pl>
2023-02-23 22:13:17 +0100

    added BMI Calculator to navigation

---
+++ src/BMIcalculatorPage/index.js
@@ -0,0 +1,11 @@
+import BMIcalculator from "./BMIcalculator";
+
+function BMIcalculatorPage() {
+  return (
+    <div>
+      <BMIcalculator />
+    </div>
+  );
+}
+
+export default BMIcalculatorPage;
