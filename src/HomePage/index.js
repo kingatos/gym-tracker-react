faba4babd1ef4958a3c087a00bae55651b7c3cc0
Kinga Tos <toskinga@interia.pl>
2023-03-01 22:00:16 +0100

    created component Home

---
+++ src/HomePage/index.js
@@ -0,0 +1,12 @@
+import "./style.css";
+
+function HomePage() {
+return (
+    <div>
+        <h2 className="welcoming">Welcome to Gym Tracker</h2>
+        <p className="welcoming__pharagraph">Know your body and save your daily progress one-click!</p>
+    </div>
+)
+};
+
+export default HomePage;
