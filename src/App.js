2e38f4e1259cc17bf7e1cbbe54ea0690f9ec11d1
Kinga Tos <toskinga@interia.pl>
2023-03-01 19:49:58 +0100

    creation modifications with new folder

--- src/App.js
+++ src/App.js
@@ -3,10 +3,9 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import BMIcalculatorPage from "./BMIcalculatorPage";
 import Container from "./Container";
 import Header from "./Header";
-import Section from "./Section";
-import FormPage from "./FormPage";
 import BurgerMenu from "./BurgerMenu";
 import trackerLogo from "./Header/images/trackerlogo.png";
+import BodyInfoPage from "./BodyInfoPage";
 
 const getInitialVisits = () => {
   const visitsFromLocalStorage = localStorage.getItem("visits");
@@ -23,42 +22,22 @@ function App() {
     localStorage.setItem("visits", JSON.stringify(visits));
   }, [visits]);
 
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    const formData = new FormData(event.target);
-    const newVisit = {
-      id: Date.now(),
-      visitDate: formData.get("visit-date"),
-      weight: formData.get("weight"),
-      biceps: formData.get("biceps"),
-      chest: formData.get("chest"),
-      waist: formData.get("waist"),
-    };
-    setVisits([...visits, newVisit]);
-  };
-
-  const removeVisit = (id) => {
-    setVisits((visits) => visits.filter((visit) => visit.id !== id));
-  };
-
   return (
     <>
-        <Container>
-          <Header trackerLogo={trackerLogo} subtitle="know your body" />
-          <div>
-            <FormPage onSubmit={handleSubmit} />
-            <br />
-            <Section visits={visits} removeVisit={removeVisit} />
-          </div>
-          <Router>
-        <Routes>
-          <Route path="/" element={<FormPage />} />
-          <Route path="/bmi-calculator" element={<BMIcalculatorPage />} />
-        </Routes>
-        <BurgerMenu />
+      <Container>
+        <Header trackerLogo={trackerLogo}  />
+        <Router>
+          <Routes>
+            <Route
+              path="/body-info"
+              element={<BodyInfoPage subtitle="know your body" visits={visits} setVisits={setVisits} />}
+            />
+            <Route path="/bmi-calculator" element={<BMIcalculatorPage />} />
+          </Routes>
+          <BurgerMenu />
         </Router>
-        </Container>
-      </>
+      </Container>
+    </>
   );
 }
 
