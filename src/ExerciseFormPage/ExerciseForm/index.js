d5a8b7da5cefe0e8e7a68c8f50563a3799fe2ddf
Kinga Tos <toskinga@interia.pl>
2023-05-20 20:41:57 +0200

    formatted code

--- src/ExerciseFormPage/ExerciseForm/index.js
+++ src/ExerciseFormPage/ExerciseForm/index.js
@@ -1,79 +1,59 @@
 import "./style.css";
 
-
 const ExerciseForm = ({ onSubmit }) => (
   <>
-        <h2 className="header">know your progress</h2>
-    <form
-      className="form"
-      onSubmit={onSubmit}
-    >
-      <label
-        className="label"
-        htmlFor="exercise-date"
-      >
-          Exercise Date:
+    <h2 className="header">know your progress</h2>
+    <form className="form" onSubmit={onSubmit}>
+      <label className="label" htmlFor="exercise-date">
+        Exercise Date:
       </label>
-    <input
-      className="input"
-      type="date"
-      id="exercise-date"
-      name="exercise-date"
-      required
-    />
-    <br />
-      <label 
-        className="label"
-        htmlFor="exercise"
-      >
-          Exercise:
-      </label>
-        <input
-          className="input"
-          htmlFor="exercise"
-          id="exercise"
-          name="exercise"
-          required
-          placeholder="enter type of exercise"
-        />
-      <br />
-      <label
-        className="label"
-        htmlFor="weight"
-      >
-          Weight:
-      </label>
-        <input
-          className="input"
-          type="number"
-          id="weight"
-          name="weight"
-          required
-          placeholder="kg"
-        />
-      <br />
-      <label 
-        className="label"
-        htmlFor="reps"
-      >
-          Repetitions/Series:
-      </label>
-        <input
-          className="input"
-          type="number"
-          id="reps"
-          name="reps"
-          required
-          placeholder="enter number of reps"
-        />
-      <br />
       <input
-        className="buttonInput"
-        type="submit"
-        value="Save results" 
+        className="input"
+        type="date"
+        id="exercise-date"
+        name="exercise-date"
+        required
       />
+      <br />
+      <label className="label" htmlFor="exercise">
+        Exercise:
+      </label>
+      <input
+        className="input"
+        htmlFor="exercise"
+        id="exercise"
+        name="exercise"
+        required
+        placeholder="enter type of exercise"
+      />
+      <br />
+      <label className="label" htmlFor="weight">
+        Weight:
+      </label>
+      <input
+        className="input"
+        type="number"
+        id="weight"
+        name="weight"
+        required
+        placeholder="kg"
+      />
+      <br />
+      <label className="label" htmlFor="reps">
+        Repetitions/Series:
+      </label>
+      <input
+        className="input"
+        type="number"
+        id="reps"
+        name="reps"
+        required
+        placeholder="enter number of reps"
+      />
+      <br />
+      <input className="buttonInput" type="submit" value="Save results" />
     </form>
   </>
-  );
+);
 
 export default ExerciseForm;
--- src/ExerciseFormPage/LastWorkout/index.js
+++ src/ExerciseFormPage/LastWorkout/index.js
@@ -1,18 +1,24 @@
 import "./style.css";
 
 const LastWorkout = ({ exercises, removeExercise }) => (
-    <div>
-      <h2 className="headerExercise">Your workout:</h2>
-        <ul className="list">
-          {exercises && 
-            exercises.map((exercise) => (
-              <li className="exerciseList" key={exercise.id}>
-              Date:{exercise.exerciseDate} Exercise: {exercise.exercise} Weight: {exercise.weight} Repetitions/Series: {exercise.reps}
-                <button onClick={() => removeExercise(exercise.id)} className="button__listRemove">✖</button>
-              </li>
-          ))}
-        </ul>
-    </div>
-  );
+  <div>
+    <h2 className="headerExercise">Your workout:</h2>
+    <ul className="list">
+      {exercises &&
+        exercises.map((exercise) => (
+          <li className="exerciseList" key={exercise.id}>
+            Date:{exercise.exerciseDate} Exercise: {exercise.exercise} Weight:{" "}
+            {exercise.weight} Repetitions/Series: {exercise.reps}
+            <button
+              onClick={() => removeExercise(exercise.id)}
+              className="button__listRemove"
+            >
+              ✖
+            </button>
+          </li>
+        ))}
+    </ul>
+  </div>
+);
 
 export default LastWorkout;
