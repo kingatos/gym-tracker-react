34de5ee3470c9d190144d7e8ca7bba9b03c366ef
Kinga Tos <toskinga@interia.pl>
2023-06-03 21:04:53 +0200

    added route path to homepage

--- src/App.js
+++ src/App.js
@@ -61,6 +61,7 @@ function App() {
                 />
               }
             />
+            <Route path="/" element={<HomePage />} />
           </Routes>
           <BurgerMenu />
         </Router>
