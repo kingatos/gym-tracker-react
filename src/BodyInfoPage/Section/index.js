0082d3a2e68fb1e5081871de18afff03f05954a5
Kinga Tos <toskinga@interia.pl>
2023-06-04 21:40:11 +0200

    formatted code

--- src/BodyInfoPage/Section/index.js
+++ src/BodyInfoPage/Section/index.js
@@ -1,23 +1,24 @@
 import "./style.css";
 
-
 const Section = ({ visits, removeVisit }) => (
-    <div>
-        <h2 className="headerVisits">Your visits:</h2>
-        <ul className="list">
-            {visits && visits.map(( visit ) => (
-                <li className="visitsList"
-                    key={visit.id}>
-                    Date: {visit.visitDate}, Weight: {visit.weight}, Biceps: {visit.biceps}, Chest: {visit.chest}, Waist: {visit.waist}
-                    <button
-                        onClick={() => removeVisit(visit.id)}
-                        className="button__listRemove">✖
-                    </button>
-
-                </li>
-            ))}
-        </ul>
-    </div>
+  <div>
+    <h2 className="headerVisits">Your visits:</h2>
+    <ul className="list">
+      {visits &&
+        visits.map((visit) => (
+          <li className="visitsList" key={visit.id}>
+            Date: {visit.visitDate}, Weight: {visit.weight}, Biceps:{" "}
+            {visit.biceps}, Chest: {visit.chest}, Waist: {visit.waist}
+            <button
+              onClick={() => removeVisit(visit.id)}
+              className="button__listRemove"
+            >
+              ✖
+            </button>
+          </li>
+        ))}
+    </ul>
+  </div>
 );
 
 export default Section;
