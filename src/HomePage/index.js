668dd0adcfda491e5db38bed7b679a7aa478b857
Kinga Tos <toskinga@interia.pl>
2023-06-21 17:59:54 +0200

    added one more word

--- src/HomePage/index.js
+++ src/HomePage/index.js
@@ -3,7 +3,7 @@ import "./style.css";
 function HomePage() {
   return (
     <div>
-      <h2 className="welcoming">Welcome to Gym Tracker</h2>
+      <h2 className="welcoming">Welcome to Gym Tracker App</h2>
       <p className="welcoming__pharagraph">
         Know your body and save your daily progress one-click!
       </p>
