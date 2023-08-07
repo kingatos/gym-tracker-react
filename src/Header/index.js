2f7ff2e9f7f148665aec770549cabfb2a5b683dc
Kinga Tos <toskinga@interia.pl>
2023-08-07 20:35:33 +0200

    formatted code

--- src/Header/index.js
+++ src/Header/index.js
@@ -1,10 +1,15 @@
 import "./style.css";
 
-
 const Header = ({ trackerLogo }) => (
-    <header>
-        <h1 className="header"><img src={trackerLogo} className="trakerImage" alt="Gym Tracker Logo"></img> </h1>
-    </header>
+  <header>
+    <h1 className="header">
+      <img
+        src={trackerLogo}
+        className="trakerImage"
+        alt="Gym Tracker Logo"
+      ></img>
+    </h1>
+  </header>
 );
 
 export default Header;
